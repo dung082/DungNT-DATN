eb85296acba2231437a63427173fe34706adcb5b
dung082 <phamnguyentiendung08022001@gmail.com>
2024-04-10 22:09:53 +0700

    pushcode

--- DungNT_DATN/src/pages/PageTemplate/PageTemplate.jsx
+++ DungNT_DATN/src/pages/PageTemplate/PageTemplate.jsx
@@ -3,44 +3,62 @@ import { Menu } from "antd";
 export default function PageTemplate() {
   const items = [
     {
-      label: "Trang chu",
+      label: "Trang chủ",
       key: "home",
     },
     {
-      label: "Thong tin chung",
+      label: "Thông tin chung",
       key: "info",
-    },
-    {
-      label: "Hoc tap",
-      key: "study",
-    },
-    {
-      label: "Thong bao",
-      key: "notice",
       children: [
         {
-          label: "Hoc tap",
-          key: "z",
+          label: "Thông tin cá nhân",
+          key: "infomation",
         },
       ],
     },
+    {
+      label: "Học tập",
+      key: "study",
+      children: [
+        {
+          label: "Thời khóa biểu",
+          key: "sheet",
+        },
+      ],
+    },
+    {
+      label: "Thông báo",
+      key: "notice",
+    },
   ];
-
+  // const { Search } = Input;
   return (
     <>
       <div className="h-screen w-screen">
         <div className="" style={{ borderBottom: "1px solid #e9e6e6" }}>
-          <div className="w-[1200px] m-auto min-h-[70px] flex justify-between items-center ">
+          <div className="w-[1300px] m-auto min-h-[60px] flex justify-between items-center ">
             <div className="flex items-center">
-              <div>
-                <span className="material-icons" style={{ fontSize: 60 }}>
-                  school
-                </span>
+              <div className="flex items-center cursor-pointer">
+                <div>
+                  <span className="material-icons" style={{ fontSize: 60 }}>
+                    school
+                  </span>
+                </div>
+                <div className="ml-2">
+                  <div className="text-xl">TRƯỜNG THCS HÒA BÌNH</div>
+                  <div>HOA BINH SECONDARY SCHOOL</div>
+                </div>
               </div>
-              <div className="ml-2">
-                <div className="text-xl">TRƯỜNG THCS HÒA BÌNH</div>
-                <div>HOA BINH SECONDARY SCHOOL</div>
-              </div>
+              {/* <div>
+                <Search
+                  placeholder="input search text"
+                  className="ml-4"
+                  // onSearch={onSearch}
+                  style={{
+                    width: 400,
+                  }}
+                />
+              </div> */}
             </div>
             <div className="flex items-center">
               <div>
@@ -53,6 +71,16 @@ export default function PageTemplate() {
               <div className="cursor-pointer ml-2">Chào mừng</div>
             </div>
           </div>
+        </div>
+
+        <div className="bg-[#f0f2f5] pt-5" style={{height : "calc(100vh - 155px"}}>
+          <div className="w-[1300px] m-auto min-h-[600px] bg-white rounded-md">
+              
+          </div>
+        </div>
+
+        <div className="text-center py-[30px] px-[70px]">
+              Một sản phẩm của DungNT
         </div>
       </div>
     </>
