45218d6db4050ba37bcfab56375dae1450d2a8f4
dung082 <phamnguyentiendung08022001@gmail.com>
2024-06-19 08:31:14 +0700

    p

---
+++ BackEndData/Migrations/20240618134357_newContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndData.Migrations
+{
+    /// <inheritdoc />
+    public partial class newContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- BackEndData/obj/Debug/net8.0/BackEndData.AssemblyInfo.cs
+++ BackEndData/obj/Debug/net8.0/BackEndData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEndData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c9b6ab776b6d6703af2bd6603faa3ed63f4fcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373f6b16dbe4e777e1a1d5a60adc212eea6a9723")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEndData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEndData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
